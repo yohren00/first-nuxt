975d246489e27783718624b98484d9237d31731a
HelloSanta <yohren00@gmail.com>
2020-08-18 18:52:11 +0800

    no message

---
+++ pages/admin/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="admin-page">
+    <section class="new-post">
+      <button @click="$router.push('/admin/new-post')">新增文章</button>
+    </section>
+    <section class="existing-posts">
+      <h1>現有文章</h1>
+      <ProstList />
+    </section>
+  </div>
+</template>
+
+<script>
+import ProstList from "../../components/Posts/PostList";
+
+export default {
+  components: {
+    ProstList,
+  },
+};
+</script>
+
+
+<style scoped>
+.admin-page {
+  padding: 20px;
+}
+
+.new-post {
+  text-align: center;
+  border-bottom: 2px solid #ccc;
+  padding-bottom: 10px;
+}
+
+.existing-posts h1 {
+  text-align: center;
+}
+</style>
